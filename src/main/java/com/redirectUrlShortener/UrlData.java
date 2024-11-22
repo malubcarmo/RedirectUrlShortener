cc886b1a981adb9007fb8d449409afda3191731d
Maria Carmo <mariabcarmo@gmail.com>
2024-11-22 20:32:38 -0300

    Add

--- src/main/java/com/redirectUrlShortener/UrlData.java
+++ src/main/java/com/redirectUrlShortener/UrlData.java
@@ -2,9 +2,11 @@ package com.redirectUrlShortener;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 
